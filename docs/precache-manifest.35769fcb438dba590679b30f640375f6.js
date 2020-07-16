f11f6176f680f938d1323ad3d925d38939e7cc62
LucasADiaz <lucasagdiaz@gmail.com>
2020-07-15 21:34:14 -0300

    html modificado

---
+++ docs/precache-manifest.35769fcb438dba590679b30f640375f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491ed72d4013ef1c442e07bd9c3fc84d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1c19483c251ab1ccba",
+    "url": "/static/css/main.950c4b26.chunk.css"
+  },
+  {
+    "revision": "4cd86be73b59bd1389dd",
+    "url": "/static/js/2.e6c90fd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6c90fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1c19483c251ab1ccba",
+    "url": "/static/js/main.d46641d5.chunk.js"
+  },
+  {
+    "revision": "a7f020f7350ffb10d26b",
+    "url": "/static/js/runtime-main.dfc84476.js"
+  }
+]);
